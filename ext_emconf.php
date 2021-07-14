028d0a22ae0e509b1b13b743bd35889fbde124d0
Christoph Lehmann <lehmann@networkteam.com>
2021-07-14 20:11:03 +0200

    Prepare release

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,14 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Pagetree Filter',
-    'description' => 'Filter the pagetree for records and content elements',
+    'description' => 'Filter the pagetree for records and content elements. For simplicity it comes with a wizard.',
     'category' => 'be',
     'state' => 'beta',
     'clearCacheOnLoad' => 1,
     'author' => 'Christoph Lehmann',
     'author_email' => 'post@christophlehmann.eu',
     'author_company' => '',
-    'version' => '0.0.1',
+    'version' => '0.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.3.99'
