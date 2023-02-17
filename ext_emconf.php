0fd768c010a477684eec09f6366541866c5da2f6
Christoph Lehmann <lehmann@networkteam.com>
2023-02-17 20:00:33 +0100

    Release 1.6.0 with TYPO3 12 Support

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Christoph Lehmann',
     'author_email' => 'post@christophlehmann.eu',
     'author_company' => '',
-    'version' => '1.5.1',
+    'version' => '1.6.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.9.99'
+            'typo3' => '11.5.0-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
